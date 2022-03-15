802feb076c1dbb4b02e665766305805eb6b4a9ca
Chris Tohill <chris@visualoverdose.ca>
2022-03-15 10:28:56 -0600

    Added new keyword to all errors. Hopefully fixes errors not being caught

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ module.exports = {
 	split(code) {
 		const parts = code.split('-')
 
-		if(parts.length !== 2) throw Error(`Invalid sync code structure. (${code})`)
+		if(parts.length !== 2) throw new Error(`Invalid sync code structure. (${code})`)
 
 		return parts
 	},
@@ -35,11 +35,11 @@ module.exports = {
 	 */
 	make(parts) {
 		if(typeof parts !== 'object' || parts == null) {
-			throw  Error("Invalid sync code parts. Must be an object containing: series, device, identifier.")
+			throw new Error("Invalid sync code parts. Must be an object containing: series, device, identifier.")
 		}
 
 		if(!parts.series || !parts.device || !parts.identifier) {
-			throw  Error("Missing sync code parts. Must be an object containing: series, device, identifier.")
+			throw new Error("Missing sync code parts. Must be an object containing: series, device, identifier.")
 		}
 
 		return this.validate([parts.series.concat(parts.device), parts.identifier].join('-'))
@@ -56,7 +56,7 @@ module.exports = {
 
 		// Throw error if series is not a single letter.
 		if(!series.match(/^[A-Za-z]{1}$/gm)) {
-			throw Error("Invalid sync code series.")
+			throw new Error("Invalid sync code series.")
 		}
 
 		return series.toUpperCase()
@@ -74,12 +74,12 @@ module.exports = {
 
 		// Throw error if device type is not a 3 character string
 		if(!device.match(/^[A-Za-z]{3}$/gm)) {
-			throw Error("Invalid sync code device type.")
+			throw new Error("Invalid sync code device type.")
 		}
 
 		// Throw error if device type is not allowed.
 		if(!this.allowedDevices.includes(device.toUpperCase())) {
-			throw Error("Device type not allowed.")
+			throw new Error("Device type not allowed.")
 		}
 
 		return device.toUpperCase()
@@ -97,7 +97,7 @@ module.exports = {
 
 		// Throw error if not a 4 digit number.
 		if(!identifier.match(/^[0-9]{4}$/gm)) {
-			throw Error("Invalid sync code identifier.")
+			throw new Error("Invalid sync code identifier.")
 		}
 
 		// Return identifier as an integer so sequences can be discovered.
