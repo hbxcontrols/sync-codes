eee030193321e01b6ec2e30d2c2c610ab3ca0f7d
Chris Tohill <chris@visualoverdose.ca>
2024-05-22 12:02:14 -0600

    Clean up code

--- src/index.js
+++ src/index.js
@@ -3,23 +3,6 @@
  * Sync code structure: Series, Device, Identifier [A][BTU]-[1234] // ABTU-1234
  */
 module.exports = {
-	allowedDevices: ['BTU', 'ENG', 'CPU', 'ECO', 'FLO', 'FLW', 'PRE', 'PRS', 'SNO', 'SUN', 'SGL', 'THM', 'ZON'],
-	deviceDescriptions: {
-		BTU: 'Energy Sensor',
-		CPU: 'Boiler Controller',
-		ECO: 'Geothermal Controller',
-		ENG: 'Energy Sensor',
-		FLO: 'Flow Sensor',
-		FLW: 'Flow Sensor',
-		PRE: 'Pressure Sensor',
-		PRS: 'Pressure Sensor',
-		SNO: 'Snow-melt Controller',
-		SUN: 'Solar Controller',
-		SGL: 'Single-zone Thermostat',
-		THM: 'Thermostat',
-		ZON: 'Zone Controller'
-	},
-
 	/**
 	 * Returns a list of device descriptions
 	 * @return {array}
@@ -44,7 +27,6 @@ module.exports = {
 		return this.allowedDevices
 	},
 
-
 	/**
 	 * Returns an array of sync code parts split at hyphen
 	 * @param  {string} A device sync code
@@ -53,7 +35,7 @@ module.exports = {
 	split(code) {
 		const parts = code.split('-')
 
-		if(parts.length !== 2) throw new Error(`Invalid sync code structure. (${code})`)
+		if (parts.length !== 2) throw new Error(`Invalid sync code structure. (${code})`)
 
 		return parts
 	},
@@ -65,11 +47,11 @@ module.exports = {
 	 * @return {string} Return a valid device sync code
 	 */
 	make(parts) {
-		if(typeof parts !== 'object' || parts == null) {
+		if (typeof parts !== 'object' || parts == null) {
 			throw new Error("Invalid sync code parts. Must be an object containing: series, device, identifier.")
 		}
 
-		if(!parts.series || !parts.device || !parts.identifier) {
+		if (!parts.series || !parts.device || !parts.identifier) {
 			throw new Error("Missing sync code parts. Must be an object containing: series, device, identifier.")
 		}
 
@@ -86,7 +68,7 @@ module.exports = {
 		const series = code.charAt(0)
 
 		// Throw error if series is not a single letter.
-		if(!series.match(/^[A-Za-z]{1}$/gm)) {
+		if (!series.match(/^[A-Za-z]{1}$/gm)) {
 			throw new Error(`Invalid sync code series. (${code})`)
 		}
 
@@ -104,12 +86,12 @@ module.exports = {
 		const device = parts[0].trim().substr(1)
 
 		// Throw error if device type is not a 3 character string
-		if(!device.match(/^[A-Za-z]{3}$/gm)) {
+		if (!device.match(/^[A-Za-z]{3}$/gm)) {
 			throw new Error(`Invalid sync code device type. (${code})`)
 		}
 
 		// Throw error if device type is not allowed.
-		if(!this.allowedDevices.includes(device.toUpperCase())) {
+		if (!this.allowedDevices.includes(device.toUpperCase())) {
 			throw new Error(`Device type not allowed. (${code})`)
 		}
 
@@ -127,7 +109,7 @@ module.exports = {
 		const identifier = parts[1].trim()
 
 		// Throw error if not a 4 digit number.
-		if(!identifier.match(/^[0-9]{4}$/gm)) {
+		if (!identifier.match(/^[0-9]{4}$/gm)) {
 			throw new Error(`Invalid sync code identifier. (${code})`)
 		}
 
@@ -159,19 +141,58 @@ module.exports = {
 	isValid(code) {
 		try {
 			return Boolean(this.validate(code))
-		} catch(error) {
+		} catch (error) {
 			return false
 		}
-	}
-
 	},
 
 
+	/**
+	 * Add a hyphen to a device sync code
+	 * @param {string} A device sync code
+	 * @returns {string} Returns a hyphenated sync code
+	 */
 	hyphenate(code) {
-		if(!code.includes('-') && code.length === 8) {
+		if (!code.includes('-') && code.length === 8) {
 			code = code.slice(0, 4) + '-' + code.slice(4)
 		}
 
 		return code
+	},
+
+	allowedDevices: ['BTU', 'ENG', 'CPU', 'ECO', 'FLO', 'FLW', 'PRE', 'PRS', 'RTR', 'SNO', 'SUN', 'SGL', 'THM', 'ZON'],
+
+	deviceDescriptions: {
+		BTU: 'Energy Sensor',
+		CPU: 'Boiler Controller',
+		ECO: 'Geothermal Controller',
+		ENG: 'Energy Sensor',
+		FLO: 'Flow Sensor',
+		FLW: 'Flow Sensor',
+		PRE: 'Pressure Sensor',
+		PRS: 'Pressure Sensor',
+		RTR: 'Access Point',
+		SNO: 'Snow-melt Controller',
+		SUN: 'Solar Controller',
+		SGL: 'Single-zone Thermostat',
+		THM: 'Thermostat',
+		ZON: 'Zone Controller'
+	},
+
+	deviceModels: {
+		BTU: 'BTU-0100',
+		CPU: 'CPU-0600',
+		ECO: 'ECO-0600',
+		ENG: 'ENG-0100',
+		FLO: 'FLO-0100',
+		FLW: 'FLW-0100',
+		PRE: 'PRE-0100',
+		PRS: 'PRS-0100',
+		RTR: 'RTR-0100',
+		SNO: 'SNO-0600',
+		SUN: 'SESF-3221',
+		SGL: 'SGL-0600',
+		THM: 'THM-0600',
+		ZON: 'ZON-0600'
 	}
 }
